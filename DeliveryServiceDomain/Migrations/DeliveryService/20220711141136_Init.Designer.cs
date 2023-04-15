e70c53359ff30ca7c6939823926696cf2fe12849
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-04-15 20:41:39 +0200

    checking pr laberer

--- DeliveryServiceDomain/Migrations/DeliveryService/20220711141136_Init.Designer.cs
+++ DeliveryServiceDomain/Migrations/DeliveryService/20220711141136_Init.Designer.cs
@@ -15,7 +15,6 @@ namespace DeliveryServiceDomain.Migrations.DeliveryService
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("ProductVersion", "5.0.14")
